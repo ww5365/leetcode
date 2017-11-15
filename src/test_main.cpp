2bcd804460e324a82bef887fee1dbcf7fde471a0
ww5365 <ww5365@163.com>
2017-11-15 18:26:49 +0800

    test code

---
+++ src/test_main.cpp
@@ -0,0 +1,25 @@
+/*
+ * test_main.cpp
+ *
+ *  Created on: Nov 15, 2017
+ *      Author: wangwei69
+ */
+
+#include <iostream>
+#include "test_main.h"
+
+using namespace std;
+
+int main(int argc,char *argv[]){
+
+    cout << "hello leetcode!" <<endl;
+
+    remove_duplicates_sort_array();
+
+
+
+    return 0;
+}
+
+
+
---
+++ src/test_main.h
@@ -0,0 +1,17 @@
+/*
+ * test_main.h
+ *
+ *  Created on: Nov 15, 2017
+ *      Author: wangwei69
+ */
+
+#ifndef TEST_MAIN_H_
+#define TEST_MAIN_H_
+
+
+void remove_duplicates_sort_array();
+
+
+
+
+#endif /* TEST_MAIN_H_ */
